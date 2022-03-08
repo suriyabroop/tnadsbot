2c34cf8e2678b253efb9ad49219bb2a39587e3ef
suriyabroop <98744069+suriyabroop@users.noreply.github.com>
2022-03-08 22:58:13 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+define('ADMIN_NAME', 'TN GAMER');
+define('ADMIN_USERNAME', '@TN_GAMING');
+define('ADMIN_CHAT_ID', 1996486622);
+define('AD_SERVICE_NAME', 'TN ADS');
+define('CHANNEL_USERNAME', 'TN_GAMER_ADS');
+define('PAYOUT_GROUP_ID',-785584753);
+define('REGISTER_BOT_USERNAME', 'TN_ADS_BOT');
+define('POST_BOT_USERNAME', '@TN_ADS_BOT');
+define('BOT_ID', '5287285087');
+
+$dbusr = 'us12er';
+$dbpas = 'pa12ss';
+$dbnam = 'tngamer';
+
+$token = '5287285087:AAHG8If-nhFJoIp_OY9Uk0DwAuMWozE6_Bw';
+
+$botcreator = '1996486622'; //BOT Maker
+$paylink = "null"; 
+$fname = "ilee"; //folder name
